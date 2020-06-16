7f205b5c906d9723854fbced7cbb760db9bd480e
Fu Diwei <fudiwei@sina.com>
2020-06-16 15:15:11 +0800

    chore: upgrade gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,6 @@ const gulp = require('gulp');
 const gulp$babel = require('gulp-babel'),
     gulp$concat = require('gulp-concat'),
     gulp$eslint = require('gulp-eslint'),
-    gulp$rename = require('gulp-rename'),
     gulp$rimraf = require('gulp-rimraf'),
     gulp$sourcemaps = require('gulp-sourcemaps'),
     gulp$strip = require('gulp-strip-debug'),
@@ -12,7 +11,8 @@ const gulp$babel = require('gulp-babel'),
 const ARGS_OUTPUTFILES = gulp$util.env.outputFiles || 'both'; // 指示输出文件类型。可取值 both / min
 
 gulp.task('clean', () => {
-    return gulp.src(['dist/**/*.js', 'dist/**/*.js.map'], {
+	return gulp
+		.src(['dist/**/*.js', 'dist/**/*.js.map'], {
             read: false
         })
         .pipe(gulp$rimraf({
@@ -21,18 +21,19 @@ gulp.task('clean', () => {
 });
 
 gulp.task('eslint', () => {
-    gulp.src(['src/**/*.js'])
+	return gulp
+		.src(['src/**/*.js'])
         .pipe(gulp$eslint())
         .pipe(gulp$eslint.format())
         .pipe(gulp$eslint.failAfterError());
 });
 
-gulp.task('dist', ['clean'], () => {
+gulp.task('dist', gulp.series('clean', () => {
     const files = ['src/**/*.js'];
     const babelOptions = {
         presets: [
             [
-                'env', {
+                '@babel/preset-env', {
                     modules: false,
                     blacklist: ['useStrict']
                 }
@@ -49,7 +50,8 @@ gulp.task('dist', ['clean'], () => {
             .pipe(gulp.dest('dist'));
     }
 
-    gulp.src(files)
+	return gulp
+		.src(files)
         .pipe(gulp$concat('index.min.js'))
         .pipe(gulp$strip())
         .pipe(gulp$sourcemaps.init())
@@ -57,8 +59,8 @@ gulp.task('dist', ['clean'], () => {
         .pipe(gulp$uglify())
         .pipe(gulp$sourcemaps.write('.'))
         .pipe(gulp.dest('dist'));
-});
+}));
 
-gulp.task('default', ['clean'], function() {
+gulp.task('default', gulp.series('clean', 'eslint', () => {
     gulp.run('dist');
-});
+}));
