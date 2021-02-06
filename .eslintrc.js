46a5cd03d7524dcab61e606108424a561d0f99c9
Fu Diwei <fudiwei@sina.com>
2021-02-06 10:38:12 +0800

    chore: prettier config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,34 +1,46 @@
 module.exports = {
-    'extends': [
-        'eslint:recommended',
-        'plugin:promise/recommended'
+    env: {
+        es6: true,
+        browser: true,
+        node: true,
+        commonjs: true,
+        amd: true,
+    },
+    extends: ['eslint:recommended'],
+    globals: {
+        wx: true,
+        my: true,
+        swan: true,
+        tt: true,
+    },
+    overrides: [
+        {
+            files: [
+                '**/__tests__/*.{j,t}s?(x)',
+                '**/tests/unit/**/*.spec.{j,t}s?(x)',
+            ],
+            env: {
+                mocha: true,
+            },
+        },
     ],
-    'env': {
-        'es6': true,
-        'browser': true,
-        'node': true,
-        'commonjs': true,
-        'amd': true
+    parserOptions: {
+        ecmaVersion: 6,
+        parser: '@typescript-eslint/parser',
+        sourceType: 'module',
     },
-    'globals': {
-        'wx': true,
-        'my': true,
-        'swan': true,
-        'tt': true
-    },
-    'parserOptions': {
-        'ecmaVersion': 6
-    },
-    'rules': {
+    plugins: ['@typescript-eslint', 'prettier'],
+    rules: {
         'indent': ['error', 4, { SwitchCase: 1 }],
         'linebreak-style': ['warn', 'windows'],
         'no-console': 'off',
         'no-extra-boolean-cast': 'off',
-        'promise/avoid-new': 'off',
+        'prettier/prettier': ['warn', { trailingComma: 'none' }],
         'quotes': ['error', 'single'],
-        'semi': ['error', 'always']
+        'semi': ['error', 'always'],
+        '@typescript-eslint/ban-ts-comment': 'warn',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/indent': ['error', 4, { VariableDeclarator: 4, SwitchCase: 1 }],
+        '@typescript-eslint/no-non-null-assertion': 'off',
     },
-    'plugins': [
-        'promise'
-    ]
 };
