fece63167da2137de5190d4b7cd0dac84e2d27d5
Fu Diwei <fudiwei@sina.com>
2021-03-18 00:37:35 +0800

    feat: 拆分工具类模块，只保留扩展方法

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,40 +1,7 @@
-import './extends-Array';
-import './extends-Date';
-import './extends-Document';
-import './extends-Function';
-import './extends-Number';
-import './extends-Promise';
-import './extends-String';
-import UtilsType from './utils-type';
-import UtilsEncoding from './utils-encoding';
-import UtilsRegex from './utils-regex';
-import UtilsStorage from './utils-storage';
-import UtilsRequest from './utils-request';
-
-// @ts-ignore
-export default {
-    /**
-     * 类型判断工具类。
-     */
-    $$: UtilsType.SKIT.JSUtils.$$,
-
-    /**
-     * 编码工具类。
-     */
-    $$encoding: UtilsEncoding.SKIT.JSUtils.$$encoding,
-
-    /**
-     * 本地存储工具类。
-     */
-    $$storage: UtilsStorage.SKIT.JSUtils.$$storage,
-
-    /**
-     * 正则表达式工具类。
-     */
-    $$regex: UtilsRegex.SKIT.JSUtils.$$regex,
-
-    /**
-     * AJAX 请求工具类。
-     */
-    $$request: UtilsRequest.SKIT.JSUtils.$$request
-};
+/// <reference path="./extends-Array.d.ts" />
+/// <reference path="./extends-Date.d.ts" />
+/// <reference path="./extends-Document.d.ts" />
+/// <reference path="./extends-Function.d.ts" />
+/// <reference path="./extends-Number.d.ts" />
+/// <reference path="./extends-Promise.d.ts" />
+/// <reference path="./extends-String.d.ts" />
