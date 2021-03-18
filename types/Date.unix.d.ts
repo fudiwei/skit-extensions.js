8e9c3df2b978f8a51aaabab48cfed8f88875540f
Fu Diwei <fudiwei@sina.com>
2021-03-18 14:14:45 +0800

    docs: 调整文档注释

--- types/Date.unix.d.ts
+++ types/Date.unix.d.ts
@@ -1,10 +1,10 @@
 interface Date {
     /**
-     * 设置 Unix 时间戳。
+     * 设置 Unix 时间戳，并返回当前 Date 对象。
      * @param {Number} timestamp Unix 时间戳（单位：秒）。
      * @return {Date}
      */
-    $unix(timestamp: number): Date;
+    $unix(timestamp: number): this;
 
     /**
      * 获取 Unix 时间戳（单位：秒）。
