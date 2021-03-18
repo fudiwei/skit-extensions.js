91acf23f1d68ef359169b40bdfe1d70144db4c35
Fu Diwei <fudiwei@sina.com>
2021-03-18 13:28:16 +0800

    docs: 调整文档注释

--- types/Array.distinct.d.ts
+++ types/Array.distinct.d.ts
@@ -1,6 +1,6 @@
 declare interface Array<T> {
     /**
-     * 返回一个仅包含去重元素的新数组。
+     * 返回一个仅包含去重元素的新数组。原始数组不会被改变。
      * @param {Function} [comparerFn] （可选）比较两个元素是否相等的函数。
      * @param {Object} [thisArg]
      * @returns {Array}
