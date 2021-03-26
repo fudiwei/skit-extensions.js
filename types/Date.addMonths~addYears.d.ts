b718166d9b7469b20321ea09f2c1f9a1d0869d92
Fu Diwei <fudiwei@sina.com>
2021-03-27 01:06:00 +0800

    feat: 补充部分 TypeScript 声明

---
+++ types/Date.addMonths~addYears.d.ts
@@ -0,0 +1,15 @@
+interface Date {
+    /**
+     * 将指定的月数加到此 Date 对象的值上。此方法会改变原 Date。
+     * @param {Number} months 月数。如果传入的是小数，将四舍五入到最接近的整数。
+     * @returns {Date}
+     */
+    $addMonths(months: number): this;
+
+    /**
+     * 将指定的年数加到此 Date 对象的值上。此方法会改变原 Date。
+     * @param {Number} years 年数。如果传入的是小数，将四舍五入到最接近的整数。
+     * @returns {Date}
+     */
+    $addYears(years: number): this;
+}
