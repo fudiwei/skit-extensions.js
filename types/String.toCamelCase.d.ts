7efccc066f0b7d9f1e2d6512c6cac1b4b6771c4c
Fu Diwei <fudiwei@sina.com>
2021-07-07 14:49:54 +0800

    feat: 新增 String.toPascalCase

--- types/String.toCamelCase.d.ts
+++ types/String.toCamelCase.d.ts
@@ -1,6 +1,6 @@
 declare interface String {
     /**
-     * 返回一个驼峰命名法（形如 `camelCase`）的新字符串。
+     * 将字符串转换为使用驼峰命名法（形如 `camelCase`）的新字符串。
      * @returns {String}
      */
     $toCamelCase(): string;
--- types/String.toSnakeCase.d.ts
+++ types/String.toSnakeCase.d.ts
@@ -1,6 +1,6 @@
 declare interface String {
     /**
-     * 返回一个蛇形命名法（形如 `snake_case`）的新字符串。
+     * 将字符串转换为使用蛇形命名法（形如 `snake_case`）的新字符串。
      * @returns {String}
      */
     $toSnakeCase(): string;
